a7b4da9378f838f1ffec522d70b0ffa829eecd89
Alaa Masoud <alaa.masoud@live.com>
2017-12-22 11:31:12 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Layout from './components/Layout';
+import './index.css';
+
+ReactDOM.render(
+  <Layout />,
+  document.getElementById('root') as HTMLElement
+);
