2fed440235d395c84260a411c57a45e7090d5d23
Alaa Masoud <alaa.masoud@live.com>
2017-12-23 10:51:36 +0300

    Add mobx dev tools on dev environment only

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Layout from './components/Layout';
+import * as mobx from 'mobx';
+import Layout from './Layout';
 import './index.css';
 
+mobx.useStrict(true);
+
+let mobxDevTools: JSX.Element | boolean = false;
+if (process.env.NODE_ENV === 'development') {
+  const DevTools = require('mobx-react-devtools').default;
+  mobxDevTools = <DevTools />;
+}
+
 ReactDOM.render(
-  <Layout />,
+  <div>{mobxDevTools}<Layout /></div>,
   document.getElementById('root') as HTMLElement
 );
